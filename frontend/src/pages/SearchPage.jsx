9a898925468dd072d4fc0c9615df2e662d7d356f
mariachiaramolinarilavoro-bit <mariachiaramolinari.lavoro@gmail.com>
2026-05-05 17:39:30 +0200

    add pages SearchPage and insert temporaly array

---
+++ frontend/src/pages/SearchPage.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+export default function SearchPage() {
+
+    const games = [
+        {
+            id: 1,
+            title: "The Legend of Zelda: Breath of the Wild",
+            genre: "Action-Adventure",
+            year: 2017,
+            price: 30.00
+        },
+        {
+            id: 2,
+            title: "Red Dead Redemption 2",
+            genre: "Open World",
+            year: 2018,
+            price: 23.50
+        },
+        {
+            id: 3,
+            title: "Hollow Knight",
+            genre: "Metroidvania",
+            year: 2017,
+            price: 49.70
+        }
+    ];
+
+    return (
+        <>
+        </>
+    )
+}
