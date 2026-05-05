e639356b200b371b2637d0c819968b3efdc93260
salv-ritrovato <salv.ritrovato@gmail.com>
2026-05-05 13:18:32 +0200

    add new components + imports, new styles

---
+++ frontend/src/components/AppFooter.jsx
@@ -0,0 +1,8 @@
+import '../styles/AppFooter.css'
+
+export default function AppFooter() {
+    
+    return (
+        <h1>footer</h1>
+    );
+}
---
+++ frontend/src/components/AppHeader.jsx
@@ -0,0 +1,8 @@
+import '../styles/AppHeader.css'
+
+export default function AppHeader() {
+
+    return (
+        <h1>header</h1>
+    );
+}
