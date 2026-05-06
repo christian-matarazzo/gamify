0aabfb832449528a1874fa4916c1bf8d4eab7f2d
LuisE Cruz <Luis.cruzlc01@gmail.com>
2026-05-06 11:04:40 +0200

    Added route Show su backend Added Page GameDetail, with logic no aesthetic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,21 +4,23 @@ import DefaultLayout from "./layouts/DefaultLayout";
 import Homepage from "./pages/Homepage";
 import SearchPage from "./pages/SearchPage";
 import AppFooter from './components/AppFooter';
-import { GamesProvider } from "./context/GamesContext"; 
+import { GamesProvider } from "./context/GamesContext";
+import GameDetail from "./pages/GameDetail";
 
 function App() {
   return (
 
-      <GamesProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route element={<DefaultLayout />}>
-              <Route path="/" element={<Homepage />} />
-              <Route path="/search" element={<SearchPage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </GamesProvider>
+    <GamesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/games/:id" element={<GameDetail />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </GamesProvider>
   )
 }
 
