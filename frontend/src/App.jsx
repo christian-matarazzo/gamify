31a4b84b9ef75a674e8714c7fe0d2243c6549654
LuisE Cruz <Luis.cruzlc01@gmail.com>
2026-05-07 12:50:22 +0200

    Added working CartContext Added CardContext in GameDetail

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,21 +7,24 @@ import CartPage from "./pages/CartPage";
 import AppFooter from './components/AppFooter';
 import { GamesProvider } from "./context/GamesContext";
 import GameDetail from "./pages/GameDetail";
+import { CartProvider } from "./context/CartContext"
 
 function App() {
   return (
 
     <GamesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/search" element={<SearchPage />} />            
-            <Route path="/games/:slug" element={<GameDetail />} />
-            <Route path="/cart" element={<CartPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <CartProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/games/:slug" element={<GameDetail />} />
+              <Route path="/cart" element={<CartPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
     </GamesProvider>
   )
 }
