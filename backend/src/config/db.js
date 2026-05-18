96aff84e49617bd11bd5c35ae5979ee634de465b
salv-ritrovato <salv.ritrovato@gmail.com>
2026-05-18 09:29:36 +0200

    rendertest1

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,16 @@
 require('dotenv').config();
-const mysql = require('mysql2'); 
-
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'gamify_db',
   waitForConnections: true,
-  connectionLimit: 10, 
-  queueLimit: 0        
+  connectionLimit: 10,
+  queueLimit: 0
 });
-
 
 pool.getConnection(function (err, connection) {
   if (err) {
@@ -21,6 +20,5 @@ pool.getConnection(function (err, connection) {
   console.log('✅ Connected to MySQL database!');
   connection.release();
 });
-
 
 module.exports = pool;
